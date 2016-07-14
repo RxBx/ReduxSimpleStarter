891067201bc9a8da60dea632c4a22cff71636c83
RxBx <rxbabcock@yahoo.com>
2016-07-13 18:28:37 -0700

    add YT search api, and create state for videos object array (from YT api)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search'; //brings in package
 import SearchBar from './components/search_bar'; //brings this code thru rel file path
 
 //from console.developers.google.com after searching for YouTube Data API v3
@@ -8,14 +8,33 @@ const API_KEY = 'AIzaSyBroEwil-Wsm4pobLlpGUIcqWVGpYCJqJU';
 
 // Create a new component. This component should produce some html
 
-const App = () => { //'fat arrow' to declare function instead of 'function() {'
-	return (
-		<div>
-			<SearchBar />
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state.videos = { [] };
 
-		</div> //JSX syntax- subset of JS, "looks like" HTML, but JSX
-	);
-} //And App is a 'class' not an instance; must create instance for use in DOM
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos }); //ES6 allows compacting this syntax from  ({videos: videos})
+		}); }
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div> //JSX syntax- subset of JS, "looks like" HTML, but JSX
+		);
+	} //And App is a 'class' not an instance; must create instance for use in DOM
+}
+
+//Old code const App = () => { //'fat arrow' to declare function instead of 'function() {'
+	//return (
+		//<div>
+			//<SearchBar />
+
+		//</div> //JSX syntax- subset of JS, "looks like" HTML, but JSX
+	//);
+//} //And App is a 'class' not an instance; must create instance for use in DOM
 
 // Take this component HTML and put it on the page (in DOM)
 
